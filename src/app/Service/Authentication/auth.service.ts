448a39642f94f67af7615fc990ccdea4626ff852
Sufail-Kalathil <sufailkalathil@gmail.com>
2020-10-01 17:23:38 +0530

    token and login updated

--- src/app/Service/Authentication/auth.service.ts
+++ src/app/Service/Authentication/auth.service.ts
@@ -1,9 +1,54 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+// @ts-ignore
+import {ip} from '../../../assets/data/ip.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  ipAddress;
+  constructor(public http: HttpClient) {
+    this.ipAddress = ip ;
 
-  constructor() { }
+  }
+
+  login(Data) {
+    const url = this.ipAddress + 'user/login/';
+    return this.http.post(url, Data);
+  }
+
+  setToLoggedIn() {
+    localStorage.setItem('isLoggedIn', 'true');
+  }
+
+  setToLoggedOut() {
+    localStorage.setItem('isLoggedIn', 'false');
+  }
+
+  getIsLoggedIn() {
+    return localStorage.getItem('isLoggedIn');
+  }
+
+  getUserDetails() {
+    return JSON.parse(localStorage.getItem('UserData'));
+  }
+
+  clearUserDetails() {
+    localStorage.removeItem('UserData');
+  }
+
+  // tslint:disable-next-line:variable-name
+  setUserDetails(user_ID, Name, is_superuser, is_staff, phone, email, token) {
+    const Data = {
+      user_id: user_ID,
+      name: Name,
+      is_superuser: is_superuser,
+      is_staff: is_staff,
+      email: email,
+      phone: phone,
+      token: token
+    };
+    localStorage.setItem('UserData', JSON.stringify(Data));
+  }
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {FormBuilder, FormGroup, FormGroupName, Validators} from '@angular/forms';
+import {AuthService} from '../../Service/Authentication/auth.service';
+import {Route, Router} from '@angular/router';
+import {HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +10,66 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  constructor() {}
+  loginForm: FormGroup;
+  validation_messages = {
+    username: [
+      {type: 'required', message: 'Username is required.'},
+    ],
+    password: [
+      {type: 'required', message: 'password is required.'},
+    ]
+  };
+
+  constructor
+  (
+    public formBuilder: FormBuilder,
+    public  authService: AuthService,
+    public  router : Router
+  ) {
+  }
 
   ngOnInit() {
+    this.setFormBuilder();
   }
+
   ngOnDestroy() {
   }
 
+  setFormBuilder() {
+    this.loginForm = this.formBuilder.group({
+      username: [
+        '',
+        [Validators.required,
+          Validators.minLength(3)]
+      ],
+      password: [
+        '',
+        [Validators.required,
+          Validators.minLength(5)]
+      ]
+    });
+  }
+
+  Login() {
+    if (this.loginForm.valid) {
+      // const fd = new FormData();
+      // Object.keys(this.loginForm.value).forEach(key => {
+      //   fd.append(key, this.loginForm.value[key]);
+      // });
+      this.authService.login(this.loginForm.value).subscribe(res => {
+
+        this.authService.setToLoggedIn();
+        this.authService.setUserDetails(res['user_id'], res['name'], false, false, res['mobile'], res['email'], res['token']);
+        this.router.navigate(['\dashboard']);
+        // tslint:disable-next-line:no-unused-expression
+      }), (error: HttpErrorResponse) => {
+        if (error.error instanceof Error) {
+          console.log('An error occurred:', error.error.message);
+        } else {
+          console.log('Backend returned status code: ', error.status);
+          console.log('Response body:', error.error);
+        }
+      };
+    }
+  }
 }
