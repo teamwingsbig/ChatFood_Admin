7c4c773af1097fab67dff195c78104e4bbb3f8c3
Sufail-Kalathil <sufailkalathil@gmail.com>
2020-11-10 23:26:56 +0530

    list product updated

--- src/app/Service/Authentication/Token/token-interceptor.service.ts
+++ src/app/Service/Authentication/Token/token-interceptor.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Router} from '@angular/router';
@@ -6,9 +6,10 @@ import {Router} from '@angular/router';
 @Injectable({
   providedIn: 'root'
 })
-export class TokenInterceptorService implements  HttpInterceptor  {
+export class TokenInterceptorService implements HttpInterceptor {
 
   Token;
+
   constructor(
     public  router: Router
   ) {
@@ -20,12 +21,33 @@ export class TokenInterceptorService implements  HttpInterceptor  {
       // this.Token = 'Token 62e6ef67ab955f81dccde9c080702c32e0675453';
     }
   }
-  intercept(req , next) {
-    const TokenReq = req.clone({
-      setHeaders: {
-        Authorization: this.Token
+
+  intercept(req, next) {
+    if (this.isValidRequestForInterceptor(req)) {
+      const TokenReq = req.clone({
+        setHeaders: {
+          Authorization: this.Token
+        }
+      });
+      return next.handle(TokenReq);
+    } else {
+      return next.handle(req);
+    }
+
+  }
+
+  private isValidRequestForInterceptor(requestUrl: string) {
+    const positionIndicator = 'live/';
+    const position = requestUrl.indexOf(positionIndicator);
+    let URL_PREFIX;
+    if (position > 0) {
+      const destination: string = requestUrl.substr(position + positionIndicator.length);
+      alert(destination);
+      if (destination === 'user/login/') {
+        return false;
+      } else {
+        return true;
       }
-    });
-    return next.handle(TokenReq);
+    }
   }
 }
