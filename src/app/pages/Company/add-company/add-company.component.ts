42a44525778fd65b69335eceea7b081d563538be
Sufail Kalathil <sufailkalathil@gmail.com>
2021-01-25 17:00:02 +0530

    add company

--- src/app/pages/Company/add-company/add-company.component.ts
+++ src/app/pages/Company/add-company/add-company.component.ts
@@ -173,7 +173,6 @@ export class AddCompanyComponent implements OnInit {
     const formData: any = new FormData();
     if (this.companyForm.valid) {
       this.spinner.show();
-      formData.append('admin_id', '');
       Object.keys(this.companyForm.value).forEach(key => {
         if (key === 'logo') {
           formData.append(key, this.LogoData);
@@ -186,7 +185,6 @@ export class AddCompanyComponent implements OnInit {
         }
       });
       this.masterService.addCompany(formData).subscribe(res => {
-        console.log(res);
         setTimeout(() => {
           let ResultSet: any;
           ResultSet = res;
